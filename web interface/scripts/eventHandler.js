efe6e032e031ca4f57dbf2a2903bdbf211f6f0d8
Yebai Zhao <zhaoyebai@gmail.com>
2016-03-29 22:17:31 -0400

    Communication methods update
    Many functions has been changed to fit a better command word channel
    between web and Photon

--- web interface/scripts/eventHandler.js
+++ web interface/scripts/eventHandler.js
@@ -3,16 +3,16 @@ var dateFromPhoton;
 function getTimePhoton(form){ //create the conncetion to the photon and get the time back, set the time in string to the webpage
     console.log("Connecting to the Photon with ID "+form.photon_ID.value);
     $.post("https://api.particle.io/v1/devices/"+form.photon_ID.value+"/webInterface?access_token="+form.photon_Token.value, 
-        {arg: "what is the time, Photon"},
+        {arg: "get T"},
         function(data){
             dateFromPhoton=new Date(data.return_value*1000);
-            document.getElementById("time_from_photon").innerHTML="Current time from the Photon is "+dateFromPhoton.toTimeString();
+            document.getElementById("time_from_photon").innerHTML="Time from the Photon: "+dateFromPhoton.toTimeString();
             console.log(data);
     });		
 }
 function syncTimePhoton(form){//force the Photon to sync the time and get the new time back to the webpage
     $.post("https://api.particle.io/v1/devices/"+form.photon_ID.value+"/webInterface?access_token="+form.photon_Token.value, 
-        {arg: "syncTime"},
+        {arg: "syncT"},
         function(data){
             dateFromPhoton=new Date(data.return_value*1000);
             document.getElementById("time_from_photon").innerHTML="Current time from the Photon is "+dateFromPhoton.toTimeString()+"(synced)";
@@ -22,7 +22,7 @@ function syncTimePhoton(form){//force the Photon to sync the time and get the ne
 }
 
 function turnMinutePhoton(form){
-    var minute_modulated=500+Number(form.turn_Minute.value);
+    var minute_modulated="minTr"+form.turn_Minute.value;
     $.post("https://api.particle.io/v1/devices/"+form.photon_ID.value+"/webTuning?access_token="+form.photon_Token.value, 
         {arg:minute_modulated},
         function(data) {
@@ -32,9 +32,20 @@ function turnMinutePhoton(form){
 }
 
 function turnHourPhoton(form){
-    var hour_modulated=1500+Number(form.turn_Hour.value);
+    var hour_modulated="houTr"+form.turn_Hour.value;
     $.post("https://api.particle.io/v1/devices/"+form.photon_ID.value+"/webTuning?access_token="+form.photon_Token.value,
         {arg: hour_modulated},
+        function(data) {
+            console.log(data);  
+        }
+    );
+}
+
+function moveServoPhoton(form){
+    var servo_modulated="ser "+form.servo_Color.value+form.servo_Percentage.value;
+    console.log(servo_modulated);
+    $.post("https://api.particle.io/v1/devices/"+form.photon_ID.value+"/webTuning?access_token="+form.photon_Token.value,
+        {arg: servo_modulated},
         function(data) {
             console.log(data);  
         }
